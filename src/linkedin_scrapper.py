42738c1b03bc6519130c160ef5d979c8dd9add41
Justin <ailton.ferrazjr@gmail.com>
2023-10-17 23:29:06 -0300

    added_changes

--- src/linkedin_scrapper.py
+++ src/linkedin_scrapper.py
@@ -1,13 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from gpt_api import CHATGPT
+
 import time
 import os, dotenv
 
 dotenv.load_dotenv()
 
+
 class LinkedinBot:
     def __init__(self, credentials: dict):
         self.credentials = credentials
+        print("1) Initializing Bot!")
         self.login_url = "https://www.linkedin.com/login"
         self.success_login = self.initate_bot()
 
@@ -24,13 +31,13 @@ class LinkedinBot:
         """Function to run the login process into linkedin"""
 
         # Access the website
-        time.sleep(1)
         self.webdriver.get(url=self.login_url)
+        time.sleep(2)
 
         # Check if we already have the login cookies in this session
         if "linkedin.com/feed/" in self.webdriver.current_url:
-            print("Login Succesfull!")
-            time.sleep(2)
+            print("2) Login Succesfull!")
+            time.sleep(0.5)
             return True
 
         time.sleep(1)
@@ -62,25 +69,37 @@ class LinkedinBot:
     def terminate_bot(self):
         self.webdriver.quit()
 
-    def expand_my_profile(self):
-        self.webdriver.get("https://www.linkedin.com/in/")
+    def scrape_my_profile(self):
+        profile_page = self.webdriver.get("https://www.linkedin.com/in/")
+        print("3) Going to profile page")
         time.sleep(5)
-        all_see_more_buttons = self.webdriver.find_elements(
-            By.CLASS_NAME, value="inline-show-more-text"
-        )
-        print(len(all_see_more_buttons))
-        for button in all_see_more_buttons:
-            time.sleep(0.5)
-            # button.click()
-            self.webdriver.execute_script("arguments[0].click();", button)
 
-    # def scrape_my_profile(self):
-    #     self.expand_my_profile()
+        # Scroll down multiple times to make sure all elements get loaded
+        for _ in range(5):  # You can adjust the range as needed
+            self.webdriver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);"
+            )
+            time.sleep(2)  # Wait for elements to load after each scroll
+
+        experience_section = self.webdriver.find_element(
+            By.XPATH, value="//main/section[7]"
+        )
+        outer_html = experience_section.get_attribute("outerHTML")
+        print(f"Experience section scrapped with {len(outer_html)} total characters")
+        return outer_html
 
 
 if __name__ == "__main__":
-    credentials = {"username": os.environ.get("EMAIL_LINKEDIN"), "password": os.environ.get("PASS_LINKEDIN")}
+    credentials = {
+        "username": os.environ.get("EMAIL_LINKEDIN"),
+        "password": os.environ.get("PASS_LINKEDIN"),
+    }
     linkedin_bot = LinkedinBot(credentials=credentials)
     login_sucessful = linkedin_bot.login_linkedin()
     if login_sucessful == True:
-        linkedin_bot.expand_my_profile()
+        my_profile = linkedin_bot.scrape_my_profile()
+        time.sleep(1)
+        print("4) Calling ChatGPT API")
+        response = CHATGPT.send_html(html=my_profile)
+        print(response)
+        # linkedin_bot.terminate_bot()
